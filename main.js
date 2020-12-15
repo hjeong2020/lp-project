2ef9ec2cfe11501ee65b97644d03725d113e60fb
hjeong2020 <64095720+hjeong2020@users.noreply.github.com>
2020-12-15 15:58:33 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+lpTag.identities = []
+lpTag.identities.push(identifyFn);
+console.log(lpTag.identities[0])
+function identifyFn(callback) {
+    callback({
+        iss: 'LP-Brand',
+        acr: 'loa1',
+        sub: "1234567890"
+    })
+}
+
+
+function authenticate(callback) {
+    console.log("The Authenticate function has been called");
+    const jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaXNzIjoiTFAtQnJhbmQiLCJhdWQiOiJhY2M6cWE1NzIyMTY3NiIsImV4cCI6MTY2MTk3NTMzMiwiaWF0IjoxNjA4MDYxNzMyfQ.tKAkTYD_rSBrlvBbz5tYbOX3tSW4-cNfuQ5rpVx5-hQ5RvMMPlYp1KqlYQf9vEIL7Ou2YTuOkyOiINjRrKKje9hp_g-p5wzv5ttfpdttg7WRvJP2EuMPutM9gFFu1wwqLyyPwPgb-7gkVF6h5SvXQONsnGa927lXD1NUV2DN-mQ";
+    callback(jwt)
+}
+
+var lpTag = window.lpTag || {};
+lpTag.sdes = lpTag.sdes || [];
+var data = {
+    type: "cart",
+    cart: choice.itemPrice,
+    title: document.title
+}
+console.log(data);
+var fun = function () { };
+lpTag.sdes.send(data, fun);
